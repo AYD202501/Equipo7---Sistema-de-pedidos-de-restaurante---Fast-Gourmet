ac711a29bcad25918be90ca16383fbc882ed2f7f
Cristian <cristian.lizarazoc@udea.edu.co>
2025-07-10 14:08:16 -0500

    Add more titles

--- src/components/atoms/titles.tsx
+++ src/components/atoms/titles.tsx
@@ -8,10 +8,22 @@ const HeroTitle = () => {
   )
 }
 
-const MediumTitle = ({ text }: { text: string }) => {
+const MediumTitle = ({ text, props }: { text: string, props?:  string}) => {
   return (
-    <h2 className="text-xl font-semibold text-center mb-2">{text}</h2>
+    <h2 className={`text-xl font-semibold text-center mb-2 ${props}`}>{text}</h2>
   )
 }
 
-export { HeroTitle, MediumTitle }
+const SmallTitle = ({ text, props }: { text: string, props?:  string}) => {
+  return (
+    <h3 className={`text-[16px] font-semibold text-left mb-2 ${props}`}>{text}</h3>
+  )
+}
+
+const SubText = ({ text, props }: { text: string, props?:  string}) => {
+  return (
+    <p className={`text-[12px] text-black font-semibold ${props}`}>{text}</p>
+  )
+}
+
+export { HeroTitle, MediumTitle, SmallTitle, SubText }
